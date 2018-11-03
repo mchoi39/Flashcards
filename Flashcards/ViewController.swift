4084f53f38a86cfcaf2fdd72bb2916975ea5ac59
Mateo Choi <mchoi39@binghamton.edu>
2018-11-03 16:05:08 -0400

    Added features

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -17,6 +17,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        backlabel.layer.cornerRadius = 20.0
+        backlabel.clipsToBounds = true
+        backlabel.layer.shadowRadius = 15.0
+        backlabel.layer.shadowOpacity = 0.2
+        frontlabel.layer.cornerRadius = 20.0
+        frontlabel.clipsToBounds = true
+        frontlabel.layer.shadowRadius = 15.0
+        frontlabel.layer.shadowOpacity = 0.2
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,7 +35,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontlabel.isHidden = true
+        if frontlabel.isHidden == true {
+            frontlabel.isHidden = false
+        }
+        else if frontlabel.isHidden == false{
+            frontlabel.isHidden = true
+        }
     }
     func updateFlashcard(question: String, answer: String) {
         frontlabel.text = question
